015402796a76ab79e2687e46cd327ff0788b8080
Maxwell Cofie <maxwell@hubtel.com>
2023-10-05 01:04:51 +0000

    added pages

--- layouts/main.vue
+++ layouts/main.vue
@@ -116,40 +116,40 @@
         </form>
         <ul class="space-y-2">
           <li>
-            <a
-                href="#"
+            <NuxtLink
+                to="/dashboard"
                 class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <img src="../assets/imgs/ic_home.svg">
               <span class="ml-3">Home</span>
-            </a>
+            </NuxtLink>
           </li>
           <li>
-            <a
-                href="#"
+            <NuxtLink
+                to="/dashboard/menu"
                 class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <img src="../assets/imgs/ic_menu.svg">
               <span class="ml-3">My Menu</span>
-            </a>
+            </NuxtLink>
           </li>
           <li>
-            <a
-                href="#"
+            <NuxtLink
+                to="/dashboard/orders"
                 class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <img src="../assets/imgs/ic_orders.svg">
               <span class="ml-3">All Orders</span>
-            </a>
+            </NuxtLink>
           </li>
           <li>
-            <a
-                href="#"
+            <NuxtLink
+                to="/dashboard/payments"
                 class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <img src="../assets/imgs/ic_payment.svg">
               <span class="ml-3">Payments</span>
-            </a>
+            </NuxtLink>
           </li>
         </ul>
         <ul
@@ -415,54 +415,7 @@
     </aside>
 
     <main class="p-4 md:ml-64 h-auto pt-20">
-      <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
-        <div
-            class="border-2 border-dashed border-gray-300 rounded-lg dark:border-gray-600 h-32 md:h-64"
-        ></div>
-        <div
-            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"
-        ></div>
-        <div
-            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"
-        ></div>
-        <div
-            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"
-        ></div>
-      </div>
-      <div
-          class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"
-      ></div>
-      <div class="grid grid-cols-2 gap-4 mb-4">
-        <div
-            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-        ></div>
-        <div
-            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-        ></div>
-        <div
-            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-        ></div>
-        <div
-            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-        ></div>
-      </div>
-      <div
-          class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"
-      ></div>
-      <div class="grid grid-cols-2 gap-4">
-        <div
-            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-        ></div>
-        <div
-            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-        ></div>
-        <div
-            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-        ></div>
-        <div
-            class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"
-        ></div>
-      </div>
+      <slot/>
     </main>
   </div>
 </template>
---
+++ pages/dashboard/orders.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-4xl">Orders</h1>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: "main",
+});
+</script>
+
+<style scoped>
+
+</style>
